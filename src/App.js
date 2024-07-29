8b7c508118e684bba46e723c311f2147ded95fc6
Izumi-Ren-agile <renizumibenesse@gmail.com>
2024-07-29 14:05:23 +0900

    Compilerのテスト

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import './css/App.css';
-import {DataFunc} from './components/DataFunc';
+import React, { useEffect, useState } from "react";
+import "./css/App.css";
+import { DataFunc } from "./components/DataFunc";
+import { CompileTest } from "./components/CompilerAsComponent";
+import {Compiler} from "./components/CompilerAsMethod"
 
 function App() {
-
   return (
     <>
-      <DataFunc collectionId={"QUESTION_CONTENT"} documentId={"1"} field={"question"}/>
+      <DataFunc
+        collectionId={"QUESTION_CONTENT"}
+        documentId={"1"}
+        field={"question"}
+      />
       {/* <DataFunc collectionId={"QUESTION_CONTENT"} field={"question"}/> */}
+      <CompileTest
+        language="java"
+        sourceCode='public class Main {
+    public static void main(String[] args) {
+    int a=1;
+    int b=10
+        System.out.println(a+b+"キリンリキ");
+    }
+}
+'
+      />
+      <p>{Compiler({language:"python3",sourceCode:'print("jsdhausdnua")'}).output}</p>
     </>
   );
 }
